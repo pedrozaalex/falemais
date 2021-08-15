1282ae4c83f835fe92a65bf44d84811508527839
Alexandre <pedrozaalexandre@gmail.com>
2021-08-14 23:11:20 -0300

    🎨 creating useRegions hook

--- src/hooks/useRegions.ts
+++ src/hooks/useRegions.ts
@@ -2,15 +2,27 @@ import { useState, useEffect } from "react";
 import { fetchWrapper } from "../helpers/fetchWrapper";
 import { Region } from "../interfaces/Regions";
 
-export default function useRegions() {
-  const [regions, setRegions] = useState<Region[] | null>(null);
+export default function useRegions(selectedDDD: number | undefined) {
+  const [regions, setRegions] = useState<Region[]>([]);
+  const [callableOptions, setCallableOptions] = useState<number[]>([]);
 
   useEffect(() => {
-    fetchWrapper
-      .get("/api/regions")
-      .then((res) => setRegions(res))
-      .catch(() => setRegions(null));
+    async function fetchRegions() {
+      const res = await fetchWrapper.get("/api/regions");
+      setRegions(res);
+    }
+
+    fetchRegions();
   }, []);
 
-  return regions;
+  useEffect(() => {
+    if (regions && regions.length) {
+      const selectedRegion = regions.find((r) => r.ddd === selectedDDD);
+      const available = selectedRegion.costs.map((c) => c.ddd);
+
+      setCallableOptions(available);
+    }
+  }, [selectedDDD]);
+
+  return { regions, callableOptions };
 }
