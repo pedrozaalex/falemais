34cb8fd2a13499e2363fae570500254ad6b5da0c
Alexandre <pedrozaalexandre@gmail.com>
2021-08-14 23:13:28 -0300

    ♻️ refactoring /api/regions endpoint for readability

--- src/pages/api/regions/index.ts
+++ src/pages/api/regions/index.ts
@@ -5,38 +5,38 @@ const servedLocations: Region[] = [
   {
     ddd: 11,
     cities: ["Região Metropolitana"],
-    costs: {
-      11: 0.19,
-      16: 0.19,
-      18: 0.19,
-    },
+    costs: [
+      { ddd: 11, cost: 0.19 },
+      { ddd: 16, cost: 0.19 },
+      { ddd: 18, cost: 0.19 },
+    ],
   },
   {
     ddd: 16,
     cities: ["Araraquara", "Franca", "Ribeirão Preto", "São Carlos"],
-    costs: {
-      11: 0.19,
-      16: 0.19,
-      17: 0.19,
-    },
+    costs: [
+      { ddd: 11, cost: 0.19 },
+      { ddd: 16, cost: 0.19 },
+      { ddd: 17, cost: 0.19 },
+    ],
   },
   {
     ddd: 17,
     cities: ["Catanduva", "Barretos", "São José do Rio Preto", "Votuporanga"],
-    costs: {
-      16: 0.19,
-      17: 0.19,
-      18: 0.19,
-    },
+    costs: [
+      { ddd: 16, cost: 0.19 },
+      { ddd: 17, cost: 0.19 },
+      { ddd: 18, cost: 0.19 },
+    ],
   },
   {
     ddd: 18,
     cities: ["Presidente Prudente", "Araçatuba", "Birigui", "Assis"],
-    costs: {
-      11: 0.19,
-      17: 0.19,
-      18: 0.19,
-    },
+    costs: [
+      { ddd: 11, cost: 0.19 },
+      { ddd: 17, cost: 0.19 },
+      { ddd: 18, cost: 0.19 },
+    ],
   },
 ];
 
