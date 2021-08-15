da6cf09e92a7b9ffa4ab8afbb3aaacef25a6e4d6
Alexandre <pedrozaalexandre@gmail.com>
2021-08-15 19:37:26 -0300

    🚧 updating data plans costs

--- src/pages/api/regions/index.ts
+++ src/pages/api/regions/index.ts
@@ -6,37 +6,25 @@ const servedLocations: Region[] = [
     ddd: 11,
     cities: ["Região Metropolitana"],
     costs: [
-      { ddd: 11, cost: 0.19 },
-      { ddd: 16, cost: 0.19 },
-      { ddd: 18, cost: 0.19 },
+      { ddd: 16, cost: 1.9 },
+      { ddd: 17, cost: 1.7 },
+      { ddd: 18, cost: 0.9 },
     ],
   },
   {
     ddd: 16,
     cities: ["Araraquara", "Franca", "Ribeirão Preto", "São Carlos"],
-    costs: [
-      { ddd: 11, cost: 0.19 },
-      { ddd: 16, cost: 0.19 },
-      { ddd: 17, cost: 0.19 },
-    ],
+    costs: [{ ddd: 11, cost: 2.9 }],
   },
   {
     ddd: 17,
     cities: ["Catanduva", "Barretos", "São José do Rio Preto", "Votuporanga"],
-    costs: [
-      { ddd: 16, cost: 0.19 },
-      { ddd: 17, cost: 0.19 },
-      { ddd: 18, cost: 0.19 },
-    ],
+    costs: [{ ddd: 11, cost: 2.7 }],
   },
   {
     ddd: 18,
     cities: ["Presidente Prudente", "Araçatuba", "Birigui", "Assis"],
-    costs: [
-      { ddd: 11, cost: 0.19 },
-      { ddd: 17, cost: 0.19 },
-      { ddd: 18, cost: 0.19 },
-    ],
+    costs: [{ ddd: 11, cost: 1.9 }],
   },
 ];
 
