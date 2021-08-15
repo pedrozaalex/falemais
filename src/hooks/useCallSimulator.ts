b497c83f0198740ebafafba90cf426c1a22dc00a
Alexandre <pedrozaalexandre@gmail.com>
2021-08-15 19:28:43 -0300

    ✨ integrating business rules into useCallSimulator

--- src/hooks/useCallSimulator.ts
+++ src/hooks/useCallSimulator.ts
@@ -10,15 +10,17 @@ export function useCallSimulator(
   if (!callerDDD || !receiverDDD || !regions) return null;
 
   const callerRegion = regions.find((r) => r.ddd === callerDDD);
-  const costPerMinute = callerRegion.costs.find(
-    (c) => c.ddd === receiverDDD
-  ).cost;
+  const receiverRegion = callerRegion.costs.find((c) => c.ddd === receiverDDD);
+
+  if (!receiverRegion) return null;
+
+  const costPerMinute = receiverRegion.cost;
 
   const costs = {
     noFM: costPerMinute * duration,
-    fm30cost: Math.max(costPerMinute * (duration - 30), 0),
-    fm60cost: Math.max(costPerMinute * (duration - 60), 0),
-    fm120cost: Math.max(costPerMinute * (duration - 120), 0),
+    fm30cost: Math.max(costPerMinute * 1.1 * (duration - 30), 0),
+    fm60cost: Math.max(costPerMinute * 1.1 * (duration - 60), 0),
+    fm120cost: Math.max(costPerMinute * 1.1 * (duration - 120), 0),
   };
 
   return costs;
